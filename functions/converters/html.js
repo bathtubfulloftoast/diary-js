0fd2d60b772649dd61d89c531fcba52c305b7adf
bathtubfulloftoast <bathtuboftoast@proton.me>
2026-02-23 13:55:37 -0700

    css urls cool too

--- functions/converters/html.js
+++ functions/converters/html.js
@@ -7,8 +7,9 @@ function entryconvert(data, date, dir) {
 var day = String(date.getDate());
 var month = String(date.getMonth() + 1); //January is 0!
 var year = date.getFullYear();
+var replace;
 
-var replace = data.replace(/(src|href|poster)\s*=\s*(?:"([^"]*)"|'([^']*)'|([^\s>]+))/g, function(match, attr, dquot, squot, nquot) {
+var htreplace = data.replace(/(src|href|poster)\s*=\s*(?:"([^"]*)"|'([^']*)'|([^\s>]+))/g, function(match, attr, dquot, squot, nquot) {
 
     let srcValue = dquot || squot || nquot;
 
@@ -26,7 +27,25 @@ var replace = data.replace(/(src|href|poster)\s*=\s*(?:"([^"]*)"|'([^']*)'|([^\s
 
 });
 
-replace = replace.replace(/<!--(.*?)-->/gs,"");
+var csreplace = htreplace.replace(/(url)\s*\(\s*(?:"([^"]*)"|'([^']*)'|([^\s>]+))\s*\)/g, function(match, attr, dquot, squot, nquot) {
+
+    let srcValue = dquot || squot || nquot;
+
+    if (!srcValue) { return match} ;
+
+    srcValue = srcValue.replace(/^(\.\/|\.\.\/)/g, "");
+
+    if(/^(http|https):\/\//g.test(srcValue) || /^#/g.test(srcValue)) {
+    return  `${attr}("${srcValue}")`;
+    } else {
+    const parse = path.parse(srcValue);
+
+    return `${attr}("${baseURL}api/file?date=${month}/${day}/${year}&dir=${dir+"/"+parse.dir}&file=` + parse.base + '")';
+    }
+
+});
+
+replace = csreplace.replace(/<!--(.*?)-->/gs,"");
 
 return replace;
 }
