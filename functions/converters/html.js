89f0c01ee2c68a2e75d8ee5b8773c33dad76e3ac
Nova <bathtuboftoast@proton.me>
2026-03-29 01:52:10 -0600

    switch to cheerio

--- functions/converters/html.js
+++ functions/converters/html.js
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import path from 'path'
+import * as cheerio from 'cheerio';
 
 const baseURL = process.env.BASEURL || "/";
 
@@ -7,47 +8,53 @@ function entryconvert(data, date, dir) {
 var day = String(date.getDate());
 var month = String(date.getMonth() + 1); //January is 0!
 var year = date.getFullYear();
-var replace;
+// var replace;
 
-var htreplace = data.replace(/(src|href|poster)\s*=\s*(?:"([^"]*)"|'([^']*)'|([^\s>]+))/g, function(match, attr, dquot, squot, nquot) {
+const $ = cheerio.load(data);
 
-    let srcValue = dquot || squot || nquot;
+$("*").each(function() {
+var osrc=$(this).attr("src");
 
-    if (!srcValue) { return match} ;
-
-    srcValue = srcValue.replace(/^(\.\/|\.\.\/)/g, "");
-
-    if(/^(http|https):\/\//g.test(srcValue) || /^#/g.test(srcValue)) {
-    return  `${attr}="${srcValue}"`;
-    } else {
-    const parse = path.parse(srcValue);
-
-    return `${attr}="${baseURL}api/file?date=${month}/${day}/${year}&dir=${dir+"/"+parse.dir}&file=` + parse.base + '"';
-    }
+if(osrc && !(/^(http|https):\/\//g.test(osrc)) ) {
+const parse = path.parse(osrc);
+var nsrc = `${baseURL}api/file?date=${month}/${day}/${year}&dir=${dir+"/"+parse.dir}&file=${parse.base}`;
+$(this).attr("src", nsrc);
+}
 
 });
 
-var csreplace = htreplace.replace(/(url)\s*\(\s*(?:"([^"]*)"|'([^']*)'|([^\s>]+))\s*\)/g, function(match, attr, dquot, squot, nquot) {
+$("*").each(function() {
+var osrc=$(this).attr("href");
 
-    let srcValue = dquot || squot || nquot;
-
-    if (!srcValue) { return match} ;
-
-    srcValue = srcValue.replace(/^(\.\/|\.\.\/)/g, "");
-
-    if(/^(http|https):\/\//g.test(srcValue) || /^#/g.test(srcValue)) {
-    return  `${attr}("${srcValue}")`;
-    } else {
-    const parse = path.parse(srcValue);
-
-    return `${attr}("${baseURL}api/file?date=${month}/${day}/${year}&dir=${dir+"/"+parse.dir}&file=` + parse.base + '")';
-    }
+if(osrc && !(/^(http|https):\/\//g.test(osrc) || /^#/g.test(osrc)) ) {
+const parse = path.parse(osrc);
+var nsrc = `${baseURL}api/file?date=${month}/${day}/${year}&dir=${dir+"/"+parse.dir}&file=${parse.base}`;
+$(this).attr("href", nsrc);
+}
 
 });
 
-replace = csreplace.replace(/<!--(.*?)-->/gs,"");
+$("video").each(function() {
+var osrc=$(this).attr("poster");
 
-return replace;
+if(osrc && !(/^(http|https):\/\//g.test(osrc)) ) {
+const parse = path.parse(osrc);
+var nsrc = `${baseURL}api/file?date=${month}/${day}/${year}&dir=${dir+"/"+parse.dir}&file=${parse.base}`;
+$(this).attr("href", nsrc);
+}
+
+});
+
+function isComment(index, node) {
+return node.type === 'comment'
+}
+$('body').contents().filter(isComment).remove();
+
+$("style").each(function() {
+$(this).replaceWith("<!--style tags arent supported, please link a css file!-->\n");
+});
+
+return $('body').html();
 }
 
 
